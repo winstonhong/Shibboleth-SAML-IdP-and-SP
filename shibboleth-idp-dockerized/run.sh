bc201a1dee30e8f61ef046b0b9b9bf1a985c6cfd
Steve Sklar <steve.sklar@hyperscience.com>
2020-10-13 17:44:56 -0400

    Final fixes

--- shibboleth-idp-dockerized/run.sh
+++ shibboleth-idp-dockerized/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker run --rm \
            -it \
+           -d \
            -v $(pwd)/ext-conf:/opt/shibboleth-idp-ext-conf \
            -e JETTY_BROWSER_SSL_KEYSTORE_PASSWORD=12345 \
            -e JETTY_BACKCHANNEL_SSL_KEYSTORE_PASSWORD=abcde \
